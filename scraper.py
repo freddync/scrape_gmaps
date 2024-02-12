4bc2b9210fc2aaeae13a3518c160a0e2bc359250
freddync <147632428+freddync@users.noreply.github.com>
2024-02-11 21:40:11 -0300

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -21,5 +21,5 @@ for option in options:
 
 driver = webdriver.Chrome(service=chrome_service, options=chrome_options)
 
-driver.get('http://nytimes.com')
+driver.get('https://maps.app.goo.gl/5mf9YyCCaBU1tNYr8')
 print(driver.title)
